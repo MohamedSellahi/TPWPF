89b9068bd54a07b89945f6d241a1a92442e027a0
Mohamed <mohamed.sellahi@gmail.com>
2017-05-23 23:50:11 +0200

    AP0

---
+++ AP0/AP0/DoubleToIntConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Data;
+
+namespace AP0 {
+  [ValueConversion(typeof(double), typeof(int))]
+  public class DoubleToIntConverter : IValueConverter {
+
+    static public DoubleToIntConverter cnvInstance = new DoubleToIntConverter();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      if (value != null) {
+        double db = (double)value;
+        return (int)db;
+      }
+      return 0;
+    
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+      throw new NotImplementedException();
+    }
+  }
+}
