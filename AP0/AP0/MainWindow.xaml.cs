f4f8c8b0d8d74ab3f85048430c123069875f0e2d
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-05-24 15:49:10 +0200

    ValidationException mechanism

--- AP0/AP0/MainWindow.xaml.cs
+++ AP0/AP0/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,50 @@ namespace AP0 {
   public partial class MainWindow : Window {
     public MainWindow() {
       InitializeComponent();
+      Style s = new Style();
+
+      
+      
     }
 
     private void Window_Loaded(object sender, RoutedEventArgs e) {
       //this.toptexBlock.Text = DateTime.Now.ToShortDateString();
+      currDateTextBlock.Text = DateTime.Now.ToString();
+      
     }
+
+    private void Button_Click(object sender, RoutedEventArgs e) {
+      if (listBox1.SelectedIndex == -1) {
+        MessageBox.Show("Choisiser une nationalité", "nationalité", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+      else {
+        string resume = textBox2.Text + " " + textBox1.Text;
+        resume += "de sexe " + (manRadioButton.IsChecked == true ? "homme" : "femme") +
+          " né" + ((manRadioButton.IsChecked == true ? "" : "e")) + " en " + ((int)slider1.Value).ToString();
+        resume += " de nationalité " + ((ListBoxItem)listBox1.SelectedItem).Content;
+        MessageBox.Show(resume);
+        
+       }
+      
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) { 
+      if (MessageBox.Show("Voulez-vous réellement quitter?", "Fermeture", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+        e.Cancel = true;
+     
+    }
+
+    private void ShowDLGBox_Click(object sender, RoutedEventArgs e) {
+      // test for creating a dlg box 
+      // create instance of window 
+      EditDialogBox dlg = new EditDialogBox("Edit Name");
+      dlg.Width = 150;
+      dlg.Height = 100;
+      dlg.ShowDialog();
+
+    }
+
+
+
   }
 }
---
+++ DialogBox/DialogBox/EditDialogWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DialogBox {
+  /// <summary>
+  /// Interaction logic for EditDialogWindow.xaml
+  /// </summary>
+  public partial class EditDialogWindow : Window {
+
+    public EditDialogWindow() {
+      InitializeComponent();
+      
+    }
+
+    
+
+    public Animal TheNewAnimal {
+      get {
+        // recover the animal instance from the resources dictionnary
+        // of the actual window , normally after updating it with valid data 
+        return (Animal)(this.Resources["AnimalInstance"]);
+      }
+    }
+
+    // Handlers 
+
+    private void OK_Button_Click(object sender, RoutedEventArgs e) {
+      this.DialogResult = true; // close the dialog box 
+    }
+
+
+
+
+
+
+  }
+
+}
